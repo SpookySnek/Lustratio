60674c08af2baa393524241bf6bcaa92001c3c0c
SpookySnek <carleric.olsson@gmail.com>
2024-12-25 17:24:18 +0100

    Added DataContext

---
+++ Lustratio.Server/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using Lustratio.Server.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lustratio.Server.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+    
+    public DbSet<Gallery> Galleries { get; set; }
+    public DbSet<Photo> Photos { get; set; }
+    public DbSet<User> Users { get; set; }
+}
